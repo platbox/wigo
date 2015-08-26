998a1c5e34a1510a9953aa9e45fc993802eec62f
Andrey Mayorov <avmayorov@platbox.com>
2015-08-27 02:19:34 +0300

    Ps: ancestry, behaviour and state, risky business

--- src/wigo_ps.erl
+++ src/wigo_ps.erl
@@ -43,18 +43,59 @@ gather_ps_(P) ->
         messages,
         message_queue_len,
         current_stacktrace,
-        binary
+        binary,
+        dictionary
     ],
     case erlang:process_info(P, Items) of
         Info when is_list(Info) ->
-            {binary, Binaries} = lists:keyfind(binary, 1, Info),
-            [[{pid, P}, {binary_memory, gather_binary_memory(Binaries)} | Info]];
+            Behaviour = gather_behaviour(Info),
+            [[
+                {pid, P},
+                {binary_memory, gather_binary_memory(Info)},
+                {ancestry, gather_ancestry(Info)},
+                {behaviour, Behaviour},
+                {state, gather_state(P, Behaviour)}
+                    | Info
+            ]];
         undefined ->
             []
     end.
 
-gather_binary_memory(Binaries) ->
-    lists:foldl(fun({_, Mem, _}, Tot) -> Mem + Tot end, 0, Binaries).
+gather_binary_memory(Info) ->
+    lists:foldl(fun({_, Mem, _}, Tot) -> Mem + Tot end, 0, kget(binary, Info)).
+
+gather_ancestry(Info) ->
+    kget('$ancestors', kget(dictionary, Info), []).
+
+gather_behaviour(Info) ->
+    InitialCall = kget('$initial_call', kget(dictionary, Info)),
+    [CurrentCall | _] = kget(current_stacktrace, Info),
+    select_behaviour(InitialCall, CurrentCall).
+
+gather_state(Pid, {supervisor, _Module}) ->
+    guard(fun () -> {children, supervisor:which_children(Pid)} end);
+gather_state(Pid, {gen_server, _Module}) ->
+    guard(fun () -> {state, sys:get_state(Pid, 100)} end);
+gather_state(Pid, {gen_fsm, _Module}) ->
+    guard(fun () -> {State, Extra} = sys:get_state(Pid, 100), {state, State, Extra} end);
+gather_state(Pid, gen_event) ->
+    guard(fun () -> {handlers, gen_event:which_handlers(Pid)} end);
+gather_state(_Pid, undefined) ->
+    undefined.
+
+guard(Fun) ->
+    try Fun() catch _:_ -> undefined end.
+
+select_behaviour({supervisor, Module, _}, _) ->
+    {supervisor, Module};
+select_behaviour({Module, init, 1}, Call) when element(1, Call) == gen_server ->
+    {gen_server, Module};
+select_behaviour({Module, init, 1}, Call) when element(1, Call) == gen_fsm ->
+    {gen_fsm, Module};
+select_behaviour({gen_event, _, _}, _) ->
+    gen_event;
+select_behaviour(_, _) ->
+    undefined.
 
 -spec load(file:name()) -> ok | {error, atom()}.
 
@@ -125,42 +166,88 @@ format_process_info(#{
     pid                := Pid,
     registered_name    := RegName,
     status             := Status,
+    ancestry           := Ancestry,
     initial_call       := InitialCall,
     memory             := Memory,
     reductions         := Reductions,
     messages           := Messages,
     binary_memory      := Binary,
-    current_stacktrace := Stacktrace
+    current_stacktrace := Stacktrace,
+    behaviour          := Behaviour,
+    state              := State
 }) ->
     Bl = "   - ",
     Pre = "        ",
-    PidString = pid_to_list(Pid),
-    Name = case RegName of [] -> PidString; _ -> [PidString, $\s, to_bin(RegName)] end,
-    [" * ", Name, nl(),
+    [" * ", format_pidname(Pid, RegName), nl(),
         Bl, "status       : ", to_bin(Status), nl(),
         Bl, "initial call : ", format_call(InitialCall), nl(),
-        Bl, "messagebox   : ", format_messages(Messages), nl(),
+        format_line_opt([Bl, "ancestry     : "], fun format_ancestry/1, Ancestry),
+        format_line_opt([Bl, "messagebox   : "], fun (M) -> format_messages(Pre, M) end, Messages),
         Bl, "memory       : ", to_bin(Memory), " bytes", nl(),
         Bl, "binaries     : ", to_bin(Binary), " bytes", nl(),
         Bl, "reductions   : ", to_bin(Reductions), nl(),
-        Bl, "stacktrace   : ", nl(), Pre, genlib_format:format_stacktrace(Stacktrace, [newlines]), nl()
+        Bl, "stacktrace   : ", nl(), Pre, genlib_format:format_stacktrace(Stacktrace, [newlines]), nl(),
+        format_line_opt([Bl, "behaviour    : "], fun format_behaviour/1, Behaviour),
+        format_line_opt([Bl], fun (S) -> format_state(Pre, S) end, State)
     ].
 
 format_call({M, F, A}) ->
     [to_bin(M), $:, to_bin(F), $/, to_bin(A)].
 
-format_messages([]) ->
-    "";
-format_messages(List) ->
+format_line_opt(_, _, []) ->
+    [];
+format_line_opt(_, _, undefined) ->
+    [];
+format_line_opt(Pre, F, List) ->
+    [Pre, F(List), nl()].
+
+format_ancestry([E]) ->
+    [format_name(E)];
+format_ancestry([E | Rest]) ->
+    [format_name(E), " <- ", format_ancestry(Rest)].
+
+format_pidname(Pid, RegName) ->
+    [pid_to_list(Pid), $\s, to_bin(RegName)].
+
+format_name(Pid) when is_pid(Pid) ->
+    pid_to_list(Pid);
+format_name(Name) when is_atom(Name) ->
+    atom_to_binary(Name, utf8).
+
+format_messages(Pre, List) ->
     Sub = lists:sublist(List, 5),
-    Pre = "        ",
     [nl(),
-        [[Pre, genlib:print(M, 80)] || M <- Sub] |
+        [[Pre, genlib:print(M, 100)] || M <- Sub] |
         case List of
             Sub -> [];
             _   -> [Pre, "... and ", to_bin(length(List) - 5), " more"]
         end
     ].
+
+format_behaviour({What, Module}) ->
+    [to_bin(What), " (via module: ", to_bin(Module), ")"];
+format_behaviour(What) ->
+    to_bin(What).
+
+format_state(Pre, What) ->
+    [pad(to_bin(element(1, What)), 12), " : ", nl() | format_state_data(Pre, What)].
+
+format_state_data(Pre, {handlers, Handlers}) ->
+    [[Pre, genlib:print(H, 100), nl()] || H <- Handlers];
+format_state_data(Pre, {children, Children}) ->
+    [[Pre, format_child(C), nl()] || C <- Children];
+format_state_data(Pre, {state, StateName, Extra}) ->
+    [Pre, genlib:print(StateName, 100), nl() | format_state_data(Pre, {state, Extra})];
+format_state_data(Pre, {state, Extra}) ->
+    [Pre, genlib:print(Extra, 800), nl()].
+
+format_child({Name, Pid, Type, _}) ->
+    [pad(to_bin(Type), 10), " : ", padl(format_sup_pid(Pid), 12), " ", genlib:print(Name, 60)].
+
+format_sup_pid(Special) when is_atom(Special) ->
+    [$<, to_bin(Special), $>];
+format_sup_pid(Pid) when is_pid(Pid) ->
+    pid_to_list(Pid).
 
 %%
 
@@ -197,6 +284,12 @@ kget(Key, List, Default) ->
         {_, Value} -> Value;
         false -> Default
     end.
+
+pad(S, N) ->
+    genlib_string:pad_string(iolist_to_binary(S), N).
+
+padl(S, N) ->
+    genlib_string:pad_left(iolist_to_binary(S), $\s, N).
 
 nl() ->
     io_lib:nl().
